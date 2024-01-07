da2f0019b19d371b397c75c1083208d9fdbde08a
Kuugang <77537302+Kuugang@users.noreply.github.com>
2024-01-07 21:24:23 +0800

    added install route

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ const express = require("express");
 const app = express();
 const port = 6969
 
+app.get("/", (req, res) => {
+    res.send("Wow")
+});
+
 app.get("/claim", (req, res) => {
     claim()
 });
 
-
-app.get("/", (req, res) => {
-    res.send("Wow")
+app.get("/install", (req, res) => {
+    install()
 });
+
 
 const server = app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
@@ -18,6 +22,21 @@ const server = app.listen(port, () => {
 
 function claim() {
     const command = 'npx playwright test index';
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error executing the command: ${error}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`Command produced an error: ${stderr}`);
+            return;
+        }
+        console.log(`Command output:\n${stdout}`);
+    });
+}
+
+function install() {
+    const command = 'npx playwright install';
     exec(command, (error, stdout, stderr) => {
         if (error) {
             console.error(`Error executing the command: ${error}`);
